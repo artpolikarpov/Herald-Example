b840c50baea30125436211cc2f87d9ce88f2f165
Kestanous <CMTopus@me.com>
2014-10-04 14:50:29 -0400

    init

---
+++ Herald-Example.js
@@ -0,0 +1,49 @@
+//Create your courier
+Herald.addCourier('clicked', {
+  media: { onsite: {} },
+  message: function () { return 'You clicked the button!'; }
+});
+
+//Create your notification
+Meteor.methods({
+  clicked: function () { 
+    if (Meteor.server) {
+      Herald.createNotification(this.userId, {courier: 'clicked'}); 
+    }
+  }
+});
+
+
+//very simple client logic
+if (Meteor.isClient) {
+  Template.hello.events({
+    'click button': function () {
+      Meteor.call('clicked', function (error, result) {});
+    }
+  });
+
+  Template.notifications.helpers({
+    notificationsCount: function () {
+      return Herald.collection.find({read: false}).count();
+    },
+    notifications: function () {
+      return Herald.collection.find({read: false});
+    } 
+  })
+
+  Template.notifications.events({
+    'click .item': function (event, template) {
+      Herald.collection.update(this._id, {$set: {read: true} });
+    }
+  });
+}
+
+
+
+// Routing and login.
+
+Router.map(function () {
+  this.route('hello', { path: '/'})
+})
+Router.onBeforeAction(AccountsTemplates.ensureSignedIn, {only: ['hello']});
+AccountsTemplates.init();
