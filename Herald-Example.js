54f82007bd02bec60f595c7acaf04a6cdcd9c17f
Kestanous <CMTopus@me.com>
2015-02-03 13:31:13 -0500

    swithced to materializecss

--- Herald-Example.js
+++ Herald-Example.js
@@ -1,14 +1,14 @@
 //Create your courier
 Herald.addCourier('clicked', {
   media: { onsite: {} },
-  message: 'stuff'
+  message: 'message'
 });
 
 //Create your notification
 Meteor.methods({
-  clicked: function () { 
+  clicked: function (text) { 
     if (Meteor.server) {
-      Herald.createNotification(this.userId, {courier: 'clicked'}); 
+      Herald.createNotification(this.userId, {courier: 'clicked', data: { text: 'text' }}); 
     }
   }
 });
@@ -17,21 +17,46 @@ Meteor.methods({
 //very simple client logic
 if (Meteor.isClient) {
   Template.hello.events({
-    'click button': function () {
-      Meteor.call('clicked', function (error, result) {});
+    'click #addNote': function () {
+      var text = ''
+      Meteor.call('clicked', text, function (error, result) {});
     }
   });
 
   Template.notifications.helpers({
     notifications: function () {
       return Herald.collection.find({read: false});
-    } 
+    },
+    message: function () {
+      return Blaze.toHTML(this.message())
+    }
   })
 
   Template.notifications.events({
     'click .remove': function (event, template) {
       Herald.collection.update(this._id, {$set: {read: true} });
     }
+  });
+
+  Template.Layout.rendered = function () {
+    $(".button-collapse").sideNav();
+  };
+
+  Template.Layout.events({
+    'click #logout': function () {
+      Meteor.logout();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function() {
+    ServiceConfiguration.configurations.remove({service: 'meteor-developer'});
+    ServiceConfiguration.configurations.insert({
+      service: 'meteor-developer',
+      clientId: '39ENpQKv3xKxyKucs',
+      secret: 'vrMXvfa2t6qAZ2PqDvihLs63iHLXgSFcYT'
+    });
   });
 }
 
@@ -56,3 +81,35 @@ AccountsTemplates.configureRoute('signIn', {
 });
 
 Router.onBeforeAction(AccountsTemplates.ensureSignedIn, {only: ['herald']});
+
+
+AccountsTemplates.configure({
+    // Behaviour
+    confirmPassword: true,
+    enablePasswordChange: true,
+    forbidClientAccountCreation: false,
+    overrideLoginErrors: true,
+    sendVerificationEmail: false,
+
+    // Appearance
+    showAddRemoveServices: true,
+    showForgotPasswordLink: true,
+    showLabels: false,
+    showPlaceholders: true,
+
+    // Client-side Validation
+    continuousValidation: true,
+    negativeFeedback: false,
+    negativeValidation: true,
+    positiveValidation: true,
+    positiveFeedback: true,
+    showValidating: true,
+
+    // Privacy Policy and Terms of Use
+    privacyUrl: 'privacy',
+    termsUrl: 'terms-of-use',
+
+    // Redirects
+    homeRoutePath: '/home',
+    redirectTimeout: 4000,
+});
